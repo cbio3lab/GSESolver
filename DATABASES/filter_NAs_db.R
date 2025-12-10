ba6b6ec43c01d559b6daefd880f9f2fe36a0b841
cbio3lab <131942260+cbio3lab@users.noreply.github.com>
2025-12-10 10:32:54 -0600

    Delete DATABASES directory

--- DATABASES/filter_NAs_db.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-
-sol <- read.csv('SolCbio3Database_merged.csv')
-
-sol_sinlogPNA <- sol %>% filter(!is.na(logPN))
-sol_sinmpNA <- sol_sinlogPNA %>% filter(!is.na(mp))
-
-write.csv(sol_sinmpNA, file = "SolCbio3Database_merged_noNA.csv")
